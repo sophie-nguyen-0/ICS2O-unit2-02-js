55006a8f6e618dfc8cab69cbfdee0a14fadc6249
sophie-nguyen-0 <sophie-nguyen-0@users.noreply.github.com>
2022-10-06 23:28:36 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -7,7 +7,10 @@
  * Do basic math.
  */
 function myButtonClicked() {
-  document.getElementById("rectangle-dimensions").innerHTML = "<p>the dimensions are:7 cm and 12 cm</p>"
-  document.getElementById("rectangle-area").innerHTML = "<p>the area is:" + (7 * 12) + "</p>"
-  document.getElementById("rectangle-perimeter").innerHTML = "<p>the perimeter is:" + (12 + 7 * 2) + "</p>"
+  document.getElementById("rectangle-dimensions").innerHTML =
+    "<p>the dimensions are:7 cm and 12 cm</p>"
+  document.getElementById("rectangle-area").innerHTML =
+    "<p>the area is:" + 7 * 12 + "</p>"
+  document.getElementById("rectangle-perimeter").innerHTML =
+    "<p>the perimeter is:" + (12 + 7 * 2) + "</p>"
 }
