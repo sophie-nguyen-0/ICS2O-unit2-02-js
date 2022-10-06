4201cb3b2667615870212fe10c2a35611de13266
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-10-06 23:26:21 +0000

    html

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//
+// Created by: sophie nguyen
+// Created on: Sep 2020
+// This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+function myButtonClicked() {
+  document.getElementById("rectangle-dimensions").innerHTML = "<p>the dimensions are:7 cm and 12 cm</p>"
+  document.getElementById("rectangle-area").innerHTML = "<p>the area is:" + (7 * 12) + "</p>"
+  document.getElementById("rectangle-perimeter").innerHTML = "<p>the perimeter is:" + (12 + 7 * 2) + "</p>"
+}
